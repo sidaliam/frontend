7b9931b16212b22463d982632861992b9cfd77b1
sidaliam <sidamoura182@gmail.com>
2025-11-30 19:50:10 +0000

    17th time

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://192.168.100.124:5000/api";
+const API_BASE_URL = "https://192.168.100.124:5000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
